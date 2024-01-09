414d0b004cd4e7f0257935cd3be05a0869da59d2
Canario <yeraysantanac@gmail.com>
2024-01-09 19:20:10 +0000

    ejercicio5

--- myApp.js
+++ myApp.js
@@ -7,11 +7,18 @@ app.use("/public", express.static(__dirname + "/public"));
 
 const absolutePath = __dirname + '/views/index.html';
 
+let json = {
+    message: "Hello json"
+}
 
 app.get("/", function(req, res) {
     res.sendFile(absolutePath);
 })
 
+app.get("/json", function(req,res) {
+
+    res.json();
+})
 
 
 
