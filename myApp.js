a3cf912c95b2cd2f914e1b8ef601960607f49ead
Canario <yeraysantanac@gmail.com>
2024-01-09 19:04:02 +0000

    ejercicio3.2

--- myApp.js
+++ myApp.js
@@ -4,13 +4,15 @@ let app = express();
 console.log("Hello World");
 
 
+const absolutePath = __dirname + '/relativePath/file.ext'
+
 
 app.get("/", function(req, res) {
     res.send('Hello Express');
 })
 
 app.get("/", function(req, res) {
-    res.sendFile(_dirname + '/views/index.html');
+    res.sendFile(absolutePath);
 })
 
 
