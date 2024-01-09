7bce22ca3ec12b6481f17afe27f1649470e43bc1
Canario <yeraysantanac@gmail.com>
2024-01-09 18:59:07 +0000

    ejercicio3.1

--- myApp.js
+++ myApp.js
@@ -3,12 +3,14 @@ let app = express();
 
 console.log("Hello World");
 
+
+
 app.get("/", function(req, res) {
     res.send('Hello Express');
 })
 
 app.get("/", function(req, res) {
-    absolutePath = __dirname + '/views/index.html'
+    res.sendFile(_dirname + '/views/index.html');
 })
 
 
