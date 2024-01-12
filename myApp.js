8e696092eb3e5e9d3014c0a623e92e61735952d7
Canario <yeraysantanac@gmail.com>
2024-01-12 18:33:18 +0000

    ejercicio6.3

--- myApp.js
+++ myApp.js
@@ -16,9 +16,9 @@ app.get("/", function(req, res) {
 app.get("/json", function(req,res) {
 
     if (process.env.MESSAGE_STYLE="uppercase") {
-        response = "Hello World".toUpperCase();
+        response = "Hello json".toUpperCase();
       } else {
-        response = "Hello World";
+        response = "Hello json";
       }
 })
 
